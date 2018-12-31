670e0f5ef5e87330f2931891ae7de419d24e4f43
ZeyadZanaty <zeyad_zanaty@hotmail.com>
2018-12-31 02:24:15 +0200

    Add Requirements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from DataReader import DataReader
 from Preprocessor import Preprocessor
 from Vectorizer import Vectorizer
 from Classifier import Classifier
-from NeuralNetwork import NeuralNetwork
+
 dr = DataReader('./datasets/training-v1/offenseval-training-v1.tsv','A')
 data,labels = dr.get_labelled_data()
 data = data[:100]
@@ -14,13 +14,7 @@ vct = Vectorizer('fasttext')
 vectors = vct.vectorize(data)
 tr_vectors,tr_labels = vectors[:len(vectors)//2], labels[:len(vectors)//2]
 tst_vectors,tst_labels = vectors[len(vectors)//2:], labels[len(vectors)//2:]
-nn= NeuralNetwork(vectors,labels)
-nn.train()
-accs = nn.test(tst_vectors,tst_labels)
-print("Eval:",accs)
-print(nn.model.metrics_names)
-
-# clf = Classifier('KNN')
-# tuned_clf = clf.tune(tr_vectors,tr_labels,{'n_neighbors':[5]},best_only=False)
-# print(tuned_clf)
-# print(clf.score(tst_vectors,tst_labels))
+clf = Classifier('KNN')
+tuned_clf = clf.tune(tr_vectors,tr_labels,{'n_neighbors':[5]},best_only=False)
+print(tuned_clf)
+print(clf.score(tst_vectors,tst_labels))
